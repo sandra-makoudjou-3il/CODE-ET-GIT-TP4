3eacd0bfd7d347e1df3fe5afc9360a1319b02885
MAKOUDJOU <makoudjs@3il.fr>
2024-11-26 16:00:59 +0100

    correction de code de l'exrcice 2,création de 2 fonctions pour l'affichage des notes et de la moyenne

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -3,19 +3,25 @@ public class GestionnaireNotes {
     // Affiche note et moyennes
     public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
         // Affichage des notes
+        affichageNotes(nomEtudiant,notes);
+
+        // Affichage de la moyenne
+        affichageMoyenne(notes);
+    }
+    public void affichageNotes(String nomEtudiant, int[] notes)
+    {
         System.out.println("Notes de " + nomEtudiant + ":");
         for (int note : notes) {
             System.out.println("- " + note);
         }
-
-        // Calcul de la somme des notes
+    }
+    
+    public void affichageMoyenne(int[] notes)
+    {
         int somme = 0;
         for (int note : notes) {
             somme += note;
         }
-
-        // Affichage de la moyenne
-        double moyenne = (double) somme / notes.length;
-        System.out.println("Moyenne : " + moyenne);
+        System.out.println("Moyenne : " + (double) somme / notes.length);
     }
 }
